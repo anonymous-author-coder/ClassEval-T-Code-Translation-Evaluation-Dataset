066567314cb12c214a96a6252208cd93057ced22
anonymous-author-coder <anonymousauthor@126.com>
2025-01-21 16:36:00 +0800

    Update and rename VectorCalculatorTest.java to VectorUtilTest.java

--- ClassEval_T/java/test/VectorUtilTest.java
+++ ClassEval_T/java/test/VectorUtilTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
 
-class VectorCalculatorTest {
+class VectorUtilTest {
 
     @Test
     void testSimilarity1() {
         double[] vector1 = {1, 1};
         double[] vector2 = {1, 0};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(0.7071067811865475, similarity, 1e-15);
     }
 
@@ -18,7 +18,7 @@ class VectorCalculatorTest {
     void testSimilarity2() {
         double[] vector1 = {1, 1};
         double[] vector2 = {0, 0};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(0.0, similarity, 1e-15);
     }
 
@@ -26,7 +26,7 @@ class VectorCalculatorTest {
     void testSimilarity3() {
         double[] vector1 = {1, 1};
         double[] vector2 = {1, 1};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(1.0, similarity, 1e-15);
     }
 
@@ -34,7 +34,7 @@ class VectorCalculatorTest {
     void testSimilarity4() {
         double[] vector1 = {1, 1, 0, 1, 0, 1, 0, 1};
         double[] vector2 = {1, 0, 0, 1, 0, 1, 0, 1};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(0.8944271909999159, similarity, 1e-15);
     }
 
@@ -42,7 +42,7 @@ class VectorCalculatorTest {
     void testSimilarity5() {
         double[] vector1 = {1, 1, 1, 1, 1, 1, 1, 1};
         double[] vector2 = {0, 0, 0, 0, 0, 0, 0, 0};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(0.0, similarity, 1e-15);
     }
 
@@ -50,7 +50,7 @@ class VectorCalculatorTest {
     void testCosineSimilarities1() {
         double[] vector1 = {1, 1};
         List<double[]> vectorsAll = List.of(new double[]{1, 0}, new double[]{1, 1});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.7071067811865475, 1.0);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -61,7 +61,7 @@ class VectorCalculatorTest {
     void testCosineSimilarities2() {
         double[] vector1 = {1, 1, 0, 0, 1, 0, 1, 0};
         List<double[]> vectorsAll = List.of(new double[]{1, 0, 0, 0, 1, 0, 1, 0}, new double[]{1, 1, 0, 1, 1, 1, 1, 0});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.8660254037844387, 0.8164965809277261);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -72,7 +72,7 @@ class VectorCalculatorTest {
     void testCosineSimilarities3() {
         double[] vector1 = {1, 1, 0, 0, 1, 0, 1, 0};
         List<double[]> vectorsAll = List.of(new double[]{1, 0, 0, 0, 1, 0, 1, 0}, new double[]{1, 1, 1, 1, 1, 1, 1, 0});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.8660254037844387, 0.7559289460184544);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -83,7 +83,7 @@ class VectorCalculatorTest {
     void testCosineSimilarities4() {
         double[] vector1 = {1, 1, 0, 0, 1, 0, 1, 0};
         List<double[]> vectorsAll = List.of(new double[]{1, 0, 0, 0, 1, 0, 1, 0}, new double[]{1, 1, 1, 1, 1, 1, 1, 1});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.8660254037844387, 0.7071067811865475);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -94,7 +94,7 @@ class VectorCalculatorTest {
     void testCosineSimilarities5() {
         double[] vector1 = {1, 1, 0, 0, 1, 0, 1, 0};
         List<double[]> vectorsAll = List.of(new double[]{1, 0, 0, 0, 1, 0, 1, 0}, new double[]{0, 1, 1, 1, 1, 1, 1, 1});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.8660254037844387, 0.5669467095138409);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -105,7 +105,7 @@ class VectorCalculatorTest {
     void testNSimilarity1() {
         List<double[]> vectorList1 = List.of(new double[]{1, 0}, new double[]{0, 1});
         List<double[]> vectorList2 = List.of(new double[]{0, 0}, new double[]{1, 1});
-        double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(1.0, similarity, 1e-15);
     }
 
@@ -113,7 +113,7 @@ class VectorCalculatorTest {
     void testNSimilarity2() {
         List<double[]> vectorList1 = List.of(new double[]{1, 1}, new double[]{0, 1});
         List<double[]> vectorList2 = List.of(new double[]{0, 0}, new double[]{1, 1});
-        double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(0.9486832980505137, similarity, 1e-15);
     }
 
@@ -121,7 +121,7 @@ class VectorCalculatorTest {
     void testNSimilarity3() {
         List<double[]> vectorList1 = List.of(new double[]{1, 0}, new double[]{1, 1});
         List<double[]> vectorList2 = List.of(new double[]{0, 0}, new double[]{1, 1});
-        double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(0.9486832980505137, similarity, 1e-15);
     }
 
@@ -129,7 +129,7 @@ class VectorCalculatorTest {
     void testNSimilarity4() {
         List<double[]> vectorList1 = List.of(new double[]{1, 0}, new double[]{0, 1});
         List<double[]> vectorList2 = List.of(new double[]{1, 0}, new double[]{1, 1});
-        double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(0.9486832980505137, similarity, 1e-15);
     }
 
@@ -137,7 +137,7 @@ class VectorCalculatorTest {
     void testNSimilarity5() {
         List<double[]> vectorList1 = List.of(new double[]{1, 0}, new double[]{0, 1});
         List<double[]> vectorList2 = List.of(new double[]{0, 1}, new double[]{1, 1});
-        double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(0.9486832980505137, similarity, 1e-15);
     }
 
@@ -146,7 +146,7 @@ class VectorCalculatorTest {
         List<double[]> vectorList1 = Collections.emptyList();
         List<double[]> vectorList2 = Collections.emptyList();
         try {
-            double similarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+            double similarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
             fail("Expected an exception to be thrown");
         } catch (Exception e) {
             // Expected
@@ -156,7 +156,7 @@ class VectorCalculatorTest {
     @Test
     void testComputeIdfWeightDict1() {
         Map<String, Double> numDict = Map.of("key1", 0.1, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(1.0033021088637848, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
@@ -164,7 +164,7 @@ class VectorCalculatorTest {
     @Test
     void testComputeIdfWeightDict2() {
         Map<String, Double> numDict = Map.of("key1", 0.2, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(0.9162907318741551, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
@@ -172,7 +172,7 @@ class VectorCalculatorTest {
     @Test
     void testComputeIdfWeightDict3() {
         Map<String, Double> numDict = Map.of("key1", 0.3, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(0.8362480242006185, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
@@ -180,7 +180,7 @@ class VectorCalculatorTest {
     @Test
     void testComputeIdfWeightDict4() {
         Map<String, Double> numDict = Map.of("key1", 0.4, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(0.7621400520468967, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
@@ -188,20 +188,20 @@ class VectorCalculatorTest {
     @Test
     void testComputeIdfWeightDict5() {
         Map<String, Double> numDict = Map.of("key1", 0.5, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(0.6931471805599453, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
 
     @Test
-    void testVectorCalculator() {
+    void testVectorUtil() {
         double[] vector1 = {1, 1};
         double[] vector2 = {1, 0};
-        double similarity = VectorCalculator.similarity(vector1, vector2);
+        double similarity = VectorUtil.similarity(vector1, vector2);
         assertEquals(0.7071067811865475, similarity, 1e-15);
 
         List<double[]> vectorsAll = List.of(new double[]{1, 0}, new double[]{1, 1});
-        List<Double> similarities = VectorCalculator.cosineSimilarities(vector1, vectorsAll);
+        List<Double> similarities = VectorUtil.cosineSimilarities(vector1, vectorsAll);
         List<Double> expected = List.of(0.7071067811865475, 1.0);
         for (int i = 0; i < similarities.size(); i++) {
             assertEquals(expected.get(i), similarities.get(i), 1e-15);
@@ -209,11 +209,11 @@ class VectorCalculatorTest {
 
         List<double[]> vectorList1 = List.of(new double[]{1, 0}, new double[]{0, 1});
         List<double[]> vectorList2 = List.of(new double[]{0, 0}, new double[]{1, 1});
-        double nSimilarity = VectorCalculator.nSimilarity(vectorList1, vectorList2);
+        double nSimilarity = VectorUtil.nSimilarity(vectorList1, vectorList2);
         assertEquals(1.0, nSimilarity, 1e-15);
 
         Map<String, Double> numDict = Map.of("key1", 0.1, "key2", 0.5);
-        Map<String, Double> res = VectorCalculator.computeIdfWeightDict(2, numDict);
+        Map<String, Double> res = VectorUtil.computeIdfWeightDict(2, numDict);
         assertEquals(1.0033021088637848, res.get("key1"), 1e-15);
         assertEquals(0.6931471805599453, res.get("key2"), 1e-15);
     }
